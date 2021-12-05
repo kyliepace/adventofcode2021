b3583eb03adcca006784325d03813d350663d8dc
Kylie Pace <kyliepace@Kylies-MBP.attlocal.net>
2021-12-05 06:54:43 -0800

    add load-data utility functions

--- src/daysofcode/5/index.ts
+++ src/daysofcode/5/index.ts
@@ -1,5 +1,23 @@
 import { loadAndSplit } from "../../utilities/load-data"
+import { straightLinesOnly } from "./straight-lines-only"
 
 export async function five(filename: string = 'src/inputs/5.txt'){
-  const data = loadAndSplit(filename)
+  const data = loadAndSplit(filename) // ['822,976 -> 822,117', '387,419 -> 387,468]
+
+  part1(data)
+}
+
+
+function part1(data){
+  // split each line into a pair of coordinates
+  const arrayOfCoordinates = splitIntoCoordinates(data)
+  // ignore any diagonal lines
+  const arrayOfStraightLines = straightLinesOnly(data)
+}
+
+function splitIntoCoordinates(data: string[]): Array<string[]>{
+  return data.slice(0, 2).map(lineString => {
+    const coords = lineString.split(' -> ')
+    return coords
+  })
 }
---
+++ src/daysofcode/5/straight-lines-only.ts
@@ -0,0 +1,7 @@
+
+/**
+ * only return lines where either x1=x2 or y1=y2
+ */
+export function straightLinesOnly(data: string[]){
+  
+}
